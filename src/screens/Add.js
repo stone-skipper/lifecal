96168bf872f317d6e2d3ac273649d53e398469ca
stone-skipper <48980449+stone-skipper@users.noreply.github.com>
2021-08-20 00:07:54 +0900

    add new style

--- src/screens/Add.js
+++ src/screens/Add.js
@@ -6,13 +6,16 @@ import ColorSketch3 from "../components/ColorSketch3";
 import ColorSketch4 from "../components/ColorSketch4";
 import ColorSketch5 from "../components/ColorSketch5";
 import Graph from "../components/Graph";
+import Graph2 from "../components/Graph2";
+import Graph3 from "../components/Graph3";
+
 
 const Add = () => {
   return (
     <div>
       {/* <ColorSketch /> */}
       {/* <ColorSketch3 /> */}
-      <Graph />
+      <Graph3/>
       {/* <ColorSketch2></ColorSketch2> */}
     </div>
   );
